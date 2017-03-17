7e501dd95f87b3dcfd1f421264b818d582a2203a
chrisheckler <hecklerchris@hotmail.com>
2017-03-16 20:21:24 -0700

    Created Person model

---
+++ form/form/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Person(models.Model):
+    contact_name = models.CharField(max_length=30)
+    contact_email = models.EmailField(max_length=30)
+    content = forms.Charfield()
