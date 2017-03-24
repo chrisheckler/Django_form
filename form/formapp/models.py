95004b5f98f1b3362097f64ac19d358dfc560e2c
chrisheckler <hecklerchris@hotmail.com>
2017-03-23 21:27:43 -0700

    Updated models.py with __str__

--- form/formapp/models.py
+++ form/formapp/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Person(models.Model):
     contact_name = models.CharField(max_length=30)
     contact_email = models.EmailField(max_length=30)
+
+    def __str__(self):
+        return '{} {}'.format(self.contact_name, self.contact_email)
